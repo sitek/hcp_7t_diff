57e042c9e611bcbbe049793c6dfc1dc3f432d470
sitek <sitek.kevin@gmail.com>
2017-06-23 14:54:10 -0400

    Pointing to preprocessed data

--- get_data.sh
+++ get_data.sh
@@ -3,15 +3,15 @@
 # based on http://www.adliska.com/2016/02/04/how-to-download-human-connectome-project-data-from-amazon-web-services-aws.html
 # KRS 2017.06.22
 
-#SBATCH --time=24:00:00
+#SBATCH --time=2:00:00
 #SBATCH --mem=30G
 #SBATCH --qos=gablab
 
 subject=100610
-diffdir=data/$subject/diffusion
+diffdir=data/$subject/T1w/
 mkdir -p $diffdir
 
-aws s3 cp s3://hcp-openaccess/HCP_1200/100610/unprocessed/7T/Diffusion \
+aws s3 cp s3://hcp-openaccess/HCP_1200/100610/T1w/ \
   $diffdir \
   --recursive \
   --region us-east-1
